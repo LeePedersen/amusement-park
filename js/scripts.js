43772fc40d2cea5a09348acd4c20540dd824ec4b
Baul Kim and Lee Pedersen <baul@>
2019-10-24 13:46:00 -0700

    add content, function, css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-
-    var height = parseInt(prompt("What is your height in inches?"));
-    if (height === "turtles") {
-      $("#turtle").show();
-    } else if (animal === "snakes") {
-      $("#snake").show();
-    } else {
-      $("#beetle").show();
-    }
-
+  var height = parseInt(prompt("What is your height in inches?"));
+  if (height >= 72) {
+    $("#speed").css("background-color", "yellow");
+    $("#mushroom").css("background-color", "yellow");
+  } else if (height >= 60) {
+    $("#mushroom").css("background-color", "yellow");
+  } else if (height <= 48){
+    $("#grass").css("background-color", "yellow");
+  } else {
+    $("#nothing").show();
+  }
 });
